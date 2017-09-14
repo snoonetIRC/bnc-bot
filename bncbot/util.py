bd5b092c847ca0f04b0f61b7f7cff043ef7f9aaf
linuxdaemon <linuxdaemon@snoonet.org>
2017-09-14 13:10:13 -0500

    Add chunk_str util func

--- bncbot/util.py
+++ bncbot/util.py
@@ -3,10 +3,15 @@ import secrets
 import string
 
 
-def gen_pass(chars: str = (string.ascii_letters + string.digits)) -> str:
+def gen_pass(chars: str = (string.ascii_letters + string.digits), length: int = 16) -> str:
     """
     Generate a password
     :param chars: The characters to use for password generation
     :return: The generated password
     """
-    return ''.join(secrets.choice(chars) for _ in range(16))
+    return ''.join(secrets.choice(chars) for _ in range(length))
+
+
+def chunk_str(text, length=256):
+    chunks = (text[i:i + length] for i in range(0, len(text), length))
+    yield from chunks
